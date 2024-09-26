e03eb07a384f097a173f8b6c63895b444cbd542e
Sangamrai10 <ghub487@gmail.com>
2024-09-26 23:16:39 +0545

    utilities for handling responses and errors

---
+++ src/utils/apiError.js
@@ -0,0 +1,17 @@
+class apiError extends Error {
+    constructor(statusCode, message = "something went wrong!", errors = [], stack) {
+        this.statusCode = statusCode,
+            this.message = message,
+            this.errors = errors,
+            this.data = null,
+            this.success = false
+
+        if (stack) {
+            this.stack = stack
+        } else {
+            Error.captureStackTrace(this,this.constructor)
+        }
+    }
+}
+
+export {apiError}
