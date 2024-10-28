8a9aee79611a35c518f8d42ecd5de1236f78530a
Sangamrai10 <ghub487@gmail.com>
2024-10-29 01:04:37 +0545

    added routes

--- src/app.js
+++ src/app.js
@@ -2,23 +2,40 @@ import express, { urlencoded } from "express";
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 
-const app=express()
+const app = express()
 app.use(cors({
-    origin: process.env.CORS_ORIGIN || "*",
-    credentials:true
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
 }))
 
+//cookie
 app.use(cookieParser())
-app.use(express.urlencoded({extended: true, limit:"16kb"}))
-app.use(express.json({
-    limit:'16kb'
+
+//read url
+app.use(express.urlencoded({
+    extended: true,
+
+    //set limit
+    limit: "16kb"
 }))
+
+//accept json data
+app.use(express.json({
+    //set limit
+    limit: '16kb'
+}))
+
+//store image
 app.use(express.static("public"))
 
 
 // routes import 
-import userRouter from "./routes/user.routes.js";
+import userRouter from './routes/user.routes.js'
 
 // routes declarations 
-app.use("/api/v1/users",userRouter)
-export {app}
+app.use("/api/v1/users", userRouter)
+
+app.get('/', (req, res) => {
+    res.send("<h1>Hello</>")
+})
+export { app }
